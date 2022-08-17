59bf70878ad0e20df23de2d990d01ed312226ea4
Alfredo <apizarro@company.com>
2022-08-17 10:30:14 -0300

    First push

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+trap signal EXIT
+trap signal SIGINT
+
+function signal()
+{
+        echo "Exited due to signal recieved."
+        exit
+}
+
+function main()
+{
+  while true;
+   do 
+     mtr -w -c 600 --no-dns ${ETCD1} & mtr -w -c 600 --no-dns ${ETCD2}  & mtr -w -c 600 --no-dns ${ETCD3} 
+   done
+}
+
+main
